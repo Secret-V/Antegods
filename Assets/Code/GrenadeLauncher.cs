25a10e9710f2814a85ccfe5c0c27d0d6339e6420
Vincent Booman <vincent_booman@msn.com>
2019-01-07 23:18:40 +0100

    Initial upload.

---
+++ Assets/Code/GrenadeLauncher.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ControlMode
+{
+    Mouse,
+    Gamepad
+}
+
+public class GrenadeLauncher : MonoBehaviour
+{
+    public GameObject grenadePrefab;
+    public Transform nozzle;
+
+    public ControlMode controlMode;
+
+    public float minimumShootForce = 50.0f;
+    public float maximumShootForce = 250.0f;
+
+    public float maxChargeTime = 3.0f;
+
+    private float chargeTimer;
+    private bool isCharging;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector2 targetPos;
+        if (controlMode == ControlMode.Mouse) targetPos = Input.mousePosition;
+        else targetPos = screenPos + new Vector2(Input.GetAxis("ShootX"), -Input.GetAxis("ShootY"));
+
+        float dx = targetPos.x - screenPos.x;
+        float dy = targetPos.y - screenPos.y;
+        float a = Mathf.Atan2(dy, dx);
+        float angle = a * Mathf.Rad2Deg;
+
+        transform.localEulerAngles = new Vector3(-angle, .0f, .0f);
+
+        if(isCharging)
+        {
+            chargeTimer += Time.deltaTime;
+
+            if(ShouldShoot())
+            {
+                float shootForce = Mathf.Lerp(minimumShootForce, maximumShootForce, Mathf.Min(chargeTimer, maxChargeTime) / maxChargeTime);
+
+                var grenadeObject = Instantiate(grenadePrefab, nozzle.position, Quaternion.identity);
+                grenadeObject.GetComponent<Rigidbody>().AddForce(new Vector3(Mathf.Cos(a), Mathf.Sin(a), .0f) * shootForce, ForceMode.Impulse);
+
+                isCharging = false;
+            }
+        }
+        else
+        {
+            chargeTimer -= Time.deltaTime;
+
+            if (ShouldStartCharging())
+            {
+                isCharging = true;
+                chargeTimer = .0f;
+            }
+        }
+    }
+
+    private bool ShouldStartCharging()
+    {
+        if (chargeTimer > .0f) return false;
+
+        if (controlMode == ControlMode.Mouse)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                return true;
+            }
+        }
+        else
+        {
+
+        }
+
+        return false;
+    }
+
+    private bool ShouldShoot()
+    {
+        if (!isCharging) return false;
+
+        if(controlMode == ControlMode.Mouse)
+        {
+            if(Input.GetMouseButtonUp(0))
+            {
+                return true;
+            }
+        }
+        else
+        {
+
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int playerID = 0;
+
+    private Rigidbody myRigidbody;
+
+    public float movementForce;
+
+    private int health;
+    public int maxHealth = 100;
+
+	void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+
+        health = maxHealth;
+	}
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+        if (health < 0) Destroy(gameObject);
+    }
+	
+	void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis(string.Format("HorizontalP{0}", playerID)), Input.GetAxis(string.Format("VerticalP{0}", playerID)), .0f) * movementForce;
+
+        myRigidbody.AddForce(movement * Time.deltaTime, ForceMode.Force);
+	}
+}
